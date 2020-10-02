7ddedd2fea411c87319320c529b9ec639af73389
nurettin.sansli <nurettin.sansli@hepsiburada.com>
2020-10-02 09:03:14 +0300

    changed feature path

--- src/test/java/lima/TestRunner.java
+++ src/test/java/lima/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/lima/MultipleProduct.feature",
+        features = "src/test/resources/lima/",
         glue = "lima",
         plugin = { "pretty", "json:target/cucumber-report.json","io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm",
                 "html:target/html/"}
